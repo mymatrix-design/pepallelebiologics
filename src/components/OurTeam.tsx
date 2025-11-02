2ef14d08c2753b058a26f150c1f59d9ff092508a
mymatrix-design <mymatrix.design@gmail.com>
2025-11-02 20:52:01 +0530

    Update OurTeam.tsx

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -2,7 +2,7 @@ export default function OurTeam() {
   return (
     <section className="max-w-7xl mx-auto px-0 sm:px-6 py-10">
       <img
-        src="/our-team.svg"   // make sure this is in /public
+           // make sure this is in /public
         alt="Meet our team"
         className="w-full h-auto"
       />
